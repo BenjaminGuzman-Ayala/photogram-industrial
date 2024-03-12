0032710c5ff66c01c8b0e732aca1c00d973bd8e8
Benjamin Guzman-Ayala <150963859+BenjaminGuzman-Ayala@users.noreply.github.com>
2024-03-12 19:43:51 +0000

    commit a comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   #comment for code review
+  #second comment for code review
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
