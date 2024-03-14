da24fe67b34cde162bc4778ba30877d59a1d524d
Benjamin Guzman-Ayala <150963859+BenjaminGuzman-Ayala@users.noreply.github.com>
2024-03-14 16:16:20 +0000

    set up users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   root "photos#index"
+  #get "/users/:id" => "users#show", as: :user
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
+  # resources :users, only: :show
   devise_for :users
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/:username" => "users#show", as: :user
 
   # Defines the root path route ("/")
   # root "articles#index"
