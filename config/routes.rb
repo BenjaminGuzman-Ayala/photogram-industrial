a489992aa686e38664a984526a5ec51434e8ebd5
Benjamin Guzman-Ayala <150963859+BenjaminGuzman-Ayala@users.noreply.github.com>
2024-03-14 17:23:01 +0000

    added tabbed interface and links to liked photos and posts

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   devise_for :users
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get ":username/liked" => "users#liked", as: :liked
+
   get "/:username" => "users#show", as: :user
 
   # Defines the root path route ("/")
